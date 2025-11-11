86bd1490b2825b6beb536f7f66dd9f9d3dcccb7e
Ayrton Munoz <a.munoz3327@gmail.com>
2025-11-11 12:36:11 -0500

    kpi/bus: Add bus_dma_tag_create builder

--- kpi/src/bus/dma.rs
+++ kpi/src/bus/dma.rs
@@ -26,14 +26,96 @@
  * SUCH DAMAGE.
  */
 
-//use crate::bindings::bus_dma_segment_t;
+use crate::bindings::{bus_addr_t, bus_dma_lock_t, bus_dma_tag_t, bus_dmamap_t, bus_size_t};
 use crate::prelude::*;
 use core::ffi::{c_int, c_void};
-use core::ops::BitOr;
+use core::ops::{BitOr, Range};
+use core::ptr::null_mut;
 
-//#[derive(Debug)]
-//pub struct DmaMem(*mut c_void, bus_size_t);
-//pub type DmaMapFn<T> = fn(CRef<T>, &[bus_dma_segment_t], c_int) -> Result<()>;
+#[must_use]
+#[derive(Debug)]
+pub struct BusDmaTagBuilder {
+    parent: BusDmaTag,
+    alignment: Option<bus_size_t>,
+    bounds: Option<bus_addr_t>,
+    addr: Option<Range<bus_addr_t>>,
+    maxsize: Option<bus_size_t>,
+    nsegments: Option<usize>,
+    maxsegsz: Option<bus_size_t>,
+    flags: Option<BusDmaFlags>,
+    lockfunc: Option<bus_dma_lock_t>,
+    lockfunc_arg: *mut c_void,
+}
+
+impl BusDmaTagBuilder {
+    pub fn alignment(mut self, alignment: bus_size_t) -> Self {
+        self.alignment = Some(alignment);
+        self
+    }
+    pub fn bounds(mut self, bounds: bus_addr_t) -> Self {
+        self.bounds = Some(bounds);
+        self
+    }
+    pub fn addr(mut self, addr: Range<bus_addr_t>) -> Self {
+        self.addr = Some(addr);
+        self
+    }
+    pub fn maxsize(mut self, maxsize: bus_size_t) -> Self {
+        self.maxsize = Some(maxsize);
+        self
+    }
+    pub fn nsegments(mut self, nsegments: usize) -> Self {
+        self.nsegments = Some(nsegments);
+        self
+    }
+    pub fn maxsegsz(mut self, maxsegsz: bus_size_t) -> Self {
+        self.maxsegsz = Some(maxsegsz);
+        self
+    }
+    pub fn flags(mut self, flags: Option<BusDmaFlags>) -> Self {
+        self.flags = flags;
+        self
+    }
+    pub fn lockfunc(mut self, func: bus_dma_lock_t, arg: *mut c_void) -> Self {
+        self.lockfunc = Some(func);
+        self.lockfunc_arg = arg;
+        self
+    }
+    /// Allocates a DMA tag and initializes it according to the arguments.
+    pub fn build(self) -> Result<BusDmaTag> {
+        let mut tag = bus_dma_tag_t::default();
+        let addr = self.addr.unwrap_or(BUS_SPACE_MAXADDR..BUS_SPACE_MAXADDR);
+        let flags = match self.flags {
+            Some(flags) => flags.0,
+            None => 0,
+        };
+        let res = unsafe {
+            bindings::bus_dma_tag_create(
+                self.parent.0,
+                self.alignment.unwrap_or(1),
+                self.bounds.unwrap_or(0),
+                addr.start,
+                addr.end,
+                None,       /* filtfunc */
+                null_mut(), /* filtfunc_arg */
+                self.maxsize.unwrap_or(unsafe { bindings::maxphys }),
+                self.nsegments.unwrap_or(1).try_into().unwrap(),
+                self.maxsegsz.unwrap_or(unsafe { bindings::maxphys }),
+                flags,
+                self.lockfunc.unwrap_or(None),
+                self.lockfunc_arg,
+                &raw mut tag,
+            )
+        };
+        if res != 0 {
+            return Err(ErrCode::from(res));
+        }
+        Ok(BusDmaTag(tag))
+    }
+}
+
+#[derive(Debug)]
+pub struct BusDmaTag(bus_dma_tag_t);
 
 #[derive(Copy, Clone, Debug)]
 pub struct BusDmaFlags(c_int);
@@ -46,15 +128,13 @@ impl BitOr<BusDmaFlags> for BusDmaFlags {
     }
 }
 
+#[derive(Debug)]
+pub struct BusDmaMap(bus_dmamap_t);
+
 pub mod wrappers {
     use super::*;
     use crate::ErrCode;
-    use bindings::{
-        bus_addr_t, bus_dma_lock_t, bus_dma_tag_t, bus_dmamap_callback_t, bus_dmamap_t, bus_size_t,
-        device_t,
-    };
-    use core::ops::Range;
-    use core::ptr::null_mut;
+    use bindings::{bus_dmamap_callback_t, bus_dmamap_t, device_t};
 
     gen_newtype! {
         BusDmaFlags,
@@ -66,47 +146,66 @@ pub mod wrappers {
         BUS_DMA_NOCACHE,
     }
 
-    pub fn bus_get_dma_tag(dev: device_t) -> bus_dma_tag_t {
-        unsafe { bindings::bus_get_dma_tag(dev) }
+    pub fn bus_get_dma_tag(dev: device_t) -> BusDmaTag {
+        BusDmaTag(unsafe { bindings::bus_get_dma_tag(dev) })
     }
 
-    /// Allocates a DMA tag and initializes it according to the arguments.
-    pub fn bus_dma_tag_create(
-        parent: bus_dma_tag_t,
-        alignment: bus_size_t,
-        bounds: bus_addr_t,
-        addr: Range<bus_addr_t>,
-        maxsize: bus_size_t,
-        nsegments: usize,
-        maxsegsz: bus_size_t,
-        flags: BusDmaFlags,
-        lockfunc: bus_dma_lock_t,
-        lockfunc_arg: *mut c_void,
-    ) -> Result<bus_dma_tag_t> {
-        let mut tag = bus_dma_tag_t::default();
+    pub fn bus_dma_tag_create(parent: BusDmaTag) -> BusDmaTagBuilder {
+        BusDmaTagBuilder {
+            parent,
+            alignment: None,
+            bounds: None,
+            addr: None,
+            maxsize: None,
+            nsegments: None,
+            maxsegsz: None,
+            flags: None,
+            lockfunc: None,
+            lockfunc_arg: null_mut(),
+        }
+    }
+
+    pub fn bus_dmamap_create(tag: BusDmaTag, flags: Option<BusDmaFlags>) -> Result<BusDmaMap> {
+        let flags = match flags {
+            Some(flags) => flags.0,
+            None => 0,
+        };
+        let mut map = bus_dmamap_t::default();
+        let res = unsafe { bindings::bus_dmamap_create(tag.0, flags, &raw mut map) };
+        if res != 0 {
+            return Err(ErrCode::from(res));
+        }
+        Ok(BusDmaMap(map))
+    }
+
+    /// Creates a mapping in device visible address space of buflen bytes of buf, associated with the DMA map map.
+    pub fn bus_dmamap_load(
+        dmat: BusDmaTag,
+        map: BusDmaMap,
+        buf: &mut [u8],
+        callback: bus_dmamap_callback_t,
+        arg: *mut c_void,
+        flags: Option<BusDmaFlags>,
+    ) -> Result<()> {
+        let flags = match flags {
+            Some(flags) => flags.0,
+            None => 0,
+        };
         let res = unsafe {
-            bindings::bus_dma_tag_create(
-                parent,
-                alignment,
-                bounds,
-                addr.start,
-                addr.end,
-                None,       /* filtfunc */
-                null_mut(), /* filtfunc_arg */
-                maxsize,
-                nsegments as c_int,
-                maxsegsz,
-                flags.0,
-                lockfunc,
-                lockfunc_arg,
-                &mut tag,
+            bindings::bus_dmamap_load(
+                dmat.0,
+                map.0,
+                buf.as_mut_ptr().cast::<c_void>(),
+                buf.len().try_into().unwrap(),
+                callback,
+                arg,
+                flags,
             )
         };
         if res != 0 {
-            Err(ErrCode::from(res))
-        } else {
-            Ok(tag)
+            return Err(ErrCode::from(res));
         }
+        Ok(())
     }
 
     /// Allocates memory that is mapped into KVA at the address returned in vaddr
@@ -122,43 +221,6 @@ pub mod wrappers {
             Err(ErrCode::from(res))
         } else {
             Ok(map)
-        }
-    }
-
-    /// Creates a mapping in device visible address space of buflen bytes of buf, associated with the DMA map map.
-    pub fn bus_dmamap_load(
-        dmat: bus_dma_tag_t,
-        map: bus_dmamap_t,
-        //buf: DmaMem,
-        buf: *mut c_void,
-        buflen: bus_size_t,
-        callback: bus_dmamap_callback_t,
-        callback_arg: *mut c_void,
-        //callback: DmaMapFn<T>,
-        //callback_arg: CRef<T>,
-        flags: Option<BusDmaFlags>,
-    ) -> Result<()> {
-        //unsafe extern "C" fn invoke_rust_fn<T>(
-        //    callback_c_arg: *mut c_void,
-        //    segs: *mut bus_dma_segment_t,
-        //    nsegs: c_int,
-        //    error: c_int,
-        //) {
-        //    let callback_rust_arg = unsafe { callback_c_arg.cast::<T>().as_ref().unwrap() };
-        //}
-        let c_flags = match flags {
-            Some(flags) => flags.0,
-            None => 0,
-        };
-        // TODO: don't leak the arg
-        //let leaked_arg = unsafe { callback_arg.leak_ref() };
-        let res = unsafe {
-            bindings::bus_dmamap_load(dmat, map, buf, buflen, callback, callback_arg, c_flags)
-        };
-        if res != 0 {
-            Err(ErrCode::from(res))
-        } else {
-            Ok(())
         }
     }
 }
