291fdabc0f649a833117c0ceb2e6f474657e396d
Ayrton Munoz <a.munoz3327@gmail.com>
2025-11-28 17:42:42 -0500

    kpi/collections: Properly impl sglist_append for &mut T

--- kpi/src/collections.rs
+++ kpi/src/collections.rs
@@ -48,41 +48,57 @@ impl_trait_for! {
     i8 i16 i32 i64 i128
 }
 
-pub trait ScatterBuf {
-    fn get_buf(&mut self) -> (*mut c_void, usize);
+pub trait Appendable: Sized {
+    type Appended: Sized;
+
+    fn append(self) -> (Self::Appended, *mut c_void, usize);
+
+    fn reconstruct(x: Self::Appended) -> Self;
 }
 
-impl<T: Pod> ScatterBuf for Box<T> {
-    fn get_buf(&mut self) -> (*mut c_void, usize) {
+impl<T: Pod> Appendable for Box<T> {
+    type Appended = Box<T>;
+
+    fn append(mut self) -> (Self, *mut c_void, usize) {
         let ptr = self.deref_mut() as *mut T;
-        (ptr.cast::<c_void>(), size_of::<T>())
+        (self, ptr.cast::<c_void>(), size_of::<T>())
+    }
+
+    fn reconstruct(x: Self) -> Self {
+        x
     }
 }
 
-impl<T: Default + Pod> ScatterBuf for Vec<T> {
-    fn get_buf(&mut self) -> (*mut c_void, usize) {
-        // TODO: Fill with default
+impl<T: Default + Pod> Appendable for Vec<T> {
+    type Appended = Vec<T>;
+
+    fn append(mut self) -> (Self, *mut c_void, usize) {
         let ptr = self.as_ptr().cast_mut();
+        // TODO: Fill with default
         self.len = self.capacity;
-        (ptr.cast::<c_void>(), self.capacity() * size_of::<T>())
+        let size = self.capacity() * size_of::<T>();
+        (self, ptr.cast::<c_void>(), size)
+    }
+
+    fn reconstruct(x: Self) -> Self {
+        x
     }
 }
 
-impl<'a, T: Pod> ScatterBuf for &'a T {
-    fn get_buf(&mut self) -> (*mut c_void, usize) {
-        let ptr = self as *mut Self;
-        (ptr.cast::<c_void>(), size_of::<T>())
+impl<'a, T: Pod> Appendable for &'a mut T {
+    type Appended = *mut T;
+
+    fn append(self) -> (Self::Appended, *mut c_void, usize) {
+        let ptr = self as *mut T;
+        (ptr, ptr.cast::<c_void>(), size_of::<T>())
+    }
+
+    fn reconstruct(ptr: Self::Appended) -> Self {
+        unsafe { ptr.as_mut().unwrap() }
     }
 }
 
-impl<'a, T: Pod> ScatterBuf for &'a mut T {
-    fn get_buf(&mut self) -> (*mut c_void, usize) {
-        let ptr = self as *mut Self;
-        (ptr.cast::<c_void>(), size_of::<T>())
-    }
-}
-
-pub type ScatterListPtr = SyncPtr<sglist>;
+type SgListPtr = SyncPtr<sglist>;
 
 /// A handle to a buffer in a scatter-gather list
 ///
@@ -90,14 +106,14 @@ pub type ScatterListPtr = SyncPtr<sglist>;
 /// aliasing pointers in the list this type does not allow access to the buffer until the list is
 /// reset. Once the list has been reset the buffer can be accessed by calling
 /// `list_buffer.get_buffer()`.
-pub struct ListBuffer<B: ScatterBuf> {
-    buffer: Option<B>,
-    list: ScatterListPtr,
+pub struct SgBuffer<B: Appendable> {
+    buffer: Option<B::Appended>,
+    list: SgListPtr,
 }
 
-impl<B: ScatterBuf> ListBuffer<B> {
+impl<B: Appendable> SgBuffer<B> {
     /// Create a handle to a buffer in a scatter-gather list.
-    pub fn new(buffer: B, sg: &ScatterList) -> Self {
+    pub fn new(buffer: B::Appended, sg: &SgList) -> Self {
         sglist_hold(sg);
         Self {
             buffer: Some(buffer),
@@ -107,7 +123,7 @@ impl<B: ScatterBuf> ListBuffer<B> {
 
     pub fn get_buffer(mut self) -> B {
         assert!(unsafe { bindings::sglist_length(self.list.as_ptr()) } == 0);
-        self.buffer.take().unwrap()
+        Appendable::reconstruct(self.buffer.take().unwrap())
         // Drop impl calls sglist_free to release the sglist refcount
     }
 
@@ -118,7 +134,7 @@ impl<B: ScatterBuf> ListBuffer<B> {
     }
 }
 
-impl<B: ScatterBuf> Drop for ListBuffer<B> {
+impl<B: Appendable> Drop for SgBuffer<B> {
     fn drop(&mut self) {
         unsafe { bindings::sglist_free(self.list.as_ptr()) };
         if self.buffer.is_some() {
@@ -129,14 +145,14 @@ impl<B: ScatterBuf> Drop for ListBuffer<B> {
     }
 }
 
-pub struct ScatterList {
-    list: ScatterListPtr,
+pub struct SgList {
+    list: SgListPtr,
 }
 
-impl ScatterList {
+impl SgList {
     pub unsafe fn from_raw(ptr: *mut sglist) -> Self {
         Self {
-            list: ScatterListPtr::new(ptr),
+            list: SgListPtr::new(ptr),
         }
     }
     pub fn new(nsegs: usize, flags: MallocFlags) -> Result<Self> {
@@ -144,7 +160,7 @@ impl ScatterList {
         if list.is_null() {
             return Err(ENOMEM);
         }
-        let list = ScatterListPtr::new(list);
+        let list = SgListPtr::new(list);
         Ok(Self { list })
     }
 
@@ -161,35 +177,32 @@ pub mod wrappers {
     use super::*;
 
     /// Creates a new scatter-gather list with `nsegs` segments.
-    pub fn sglist_alloc(nsegs: usize, flags: MallocFlags) -> Result<ScatterList> {
-        ScatterList::new(nsegs, flags)
+    pub fn sglist_alloc(nsegs: usize, flags: MallocFlags) -> Result<SgList> {
+        SgList::new(nsegs, flags)
     }
 
     /// Append the virtual addresses spanned by `buffer` to the scatter-gather list.
     ///
-    /// This returns a `ListBuffer` to access the buffer after the list has been reset.
-    pub fn sglist_append<B: ScatterBuf>(
-        sg: &mut ScatterList,
-        mut buffer: B,
-    ) -> Result<ListBuffer<B>> {
-        let (ptr, size) = buffer.get_buf();
+    /// This returns a `SgBuffer` to access the buffer after the list has been reset.
+    pub fn sglist_append<B: Appendable>(sg: &mut SgList, mut buffer: B) -> Result<SgBuffer<B>> {
+        let (buffer, ptr, size) = buffer.append();
         let res = unsafe { bindings::sglist_append(sg.list.as_ptr(), ptr, size) };
         if res != 0 {
             forget(buffer);
             return Err(ErrCode::from(res));
         }
-        Ok(ListBuffer::new(buffer, sg))
+        Ok(SgBuffer::new(buffer, sg))
     }
 
-    pub fn sglist_reset(sg: &mut ScatterList) {
+    pub fn sglist_reset(sg: &mut SgList) {
         unsafe { bindings::sglist_reset(sg.list.as_ptr()) }
     }
 
-    pub fn sglist_length(sg: &ScatterList) -> usize {
+    pub fn sglist_length(sg: &SgList) -> usize {
         unsafe { bindings::sglist_length(sg.list.as_ptr()) }
     }
 
-    pub fn sglist_hold(sg: &ScatterList) {
+    pub fn sglist_hold(sg: &SgList) {
         unsafe { bindings::sglist_hold(sg.list.as_ptr()) };
     }
 }
