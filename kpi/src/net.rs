3b933891cdc4e3c47cc2f81dcde1fdf9d899dbb3
Ayrton Munoz <a.munoz3327@gmail.com>
2026-07-13 12:12:15 -0400

    kpi/net: More SocketRef

--- kpi/src/net.rs
+++ kpi/src/net.rs
@@ -6,6 +6,7 @@ use crate::kobj::AsRustType;
 use crate::boxed::Box;
 use core::marker::PhantomData;
 use core::ptr::NonNull;
+use core::sync::atomic::{AtomicU16, Ordering};
 
 pub type SockAddr = bindings::sockaddr;
 
@@ -45,35 +46,94 @@ pub struct SockLockGuard<'a> {
     so_lock: *mut bindings::mtx,
     pub so_options: &'a mut i32,
     pub so_state: &'a mut i16,
+    pub so_label: &'a mut *mut bindings::label,
+    pub so_dtor: &'a mut Option<unsafe extern "C" fn(*mut bindings::socket)>,
+    pub so_splice: &'a mut *mut bindings::so_splice,
+    pub so_splice_back: &'a mut *mut bindings::so_splice,
 }
 
 impl<'a> Drop for SockLockGuard<'a> {
     fn drop(&mut self) {
-        unsafe { bindings::__mtx_unlock_flags(self.so_lock.cast::<usize>(), 0, c"".as_ptr(), 0) };
+        unsafe { bindings::fn_mtx_unlock(self.so_lock) };
     }
 }
 
 // sys/socketvar.h contains a comment with what locks each struct socket field. That key is used to
 // define the following methods
 impl<'a> SocketRef<'a> {
-    pub fn get_type(&self) -> i32 {
+    pub fn so_type(&self) -> i32 {
         let so_ptr = self.0.as_ptr();
         // SAFETY: constant after allocation, no locking required.
         i32::from(unsafe { (*so_ptr).so_type })
     }
 
-    pub fn sock_lock(&self) -> SockLockGuard<'_> {
+    // TODO: Can I return a shared reference?
+    pub fn so_cred(&self) -> *mut bindings::ucred {
+        let so_ptr = self.0.as_ptr();
+        // SAFETY: constant after allocation, no locking required.
+        unsafe { (*so_ptr).so_cred }
+    }
+
+    // TODO: Can I return a shared reference?
+    pub fn so_vnet(&self) -> *mut bindings::vnet {
+        let so_ptr = self.0.as_ptr();
+        // SAFETY: constant after allocation, no locking required.
+        unsafe { (*so_ptr).so_vnet }
+    }
+
+    // TODO: Can I return a shared reference?
+    pub fn so_proto(&self) -> *mut bindings::protosw {
+        let so_ptr = self.0.as_ptr();
+        // SAFETY: constant after allocation, no locking required.
+        unsafe { (*so_ptr).so_proto }
+    }
+
+    pub fn so_error(&self) -> u16 {
+        let so_ptr = self.0.as_ptr();
+        let so_error_ptr = unsafe { &raw mut (*so_ptr).so_error };
+        // SAFETY: field is aligned to u16 and only used in other atomic u16 accesses
+        unsafe { AtomicU16::from_ptr(so_error_ptr).load(Ordering::Relaxed) }
+    }
+
+    pub fn so_rerror(&self) -> u16 {
+        let so_ptr = self.0.as_ptr();
+        let so_rerror_ptr = unsafe { &raw mut (*so_ptr).so_rerror };
+        // SAFETY: field is aligned to u16 and only used in other atomic u16 accesses
+        unsafe { AtomicU16::from_ptr(so_rerror_ptr).load(Ordering::Relaxed) }
+    }
+
+    fn sock_lock(&self) -> SockLockGuard<'_> {
         let so_ptr = self.0.as_ptr();
         let so_lock = unsafe { &raw mut (*so_ptr).so_lock };
-        unsafe { bindings::__mtx_lock_flags(so_lock.cast::<usize>(), 0, c"".as_ptr(), 0) };
+
+        unsafe { bindings::fn_mtx_lock(so_lock) };
+
         let so_options_ptr = unsafe { &raw mut (*so_ptr).so_options };
         let so_options = unsafe { so_options_ptr.as_mut().unwrap() };
+
         let so_state_ptr = unsafe { &raw mut (*so_ptr).so_state };
         let so_state = unsafe { so_state_ptr.as_mut().unwrap() };
+
+        let so_label_ptr = unsafe { &raw mut (*so_ptr).so_label };
+        let so_label = unsafe { so_label_ptr.as_mut().unwrap() };
+
+        let so_dtor_ptr =  unsafe { &raw mut (*so_ptr).so_dtor };
+        let so_dtor = unsafe { so_dtor_ptr.as_mut().unwrap() };
+
+        let so_splice_ptr =  unsafe { &raw mut (*so_ptr).so_splice };
+        let so_splice = unsafe { so_splice_ptr.as_mut().unwrap() };
+
+        let so_splice_back_ptr =  unsafe { &raw mut (*so_ptr).so_splice_back };
+        let so_splice_back = unsafe { so_splice_back_ptr.as_mut().unwrap() };
+
         SockLockGuard {
             so_lock,
             so_options,
             so_state,
+            so_label,
+            so_dtor,
+            so_splice,
+            so_splice_back,
         }
     }
 }
@@ -81,6 +141,15 @@ impl<'a> SocketRef<'a> {
 impl<'a> AsRustType<'a, SocketRef<'a>> for *mut bindings::socket {
     fn as_rust_type(&'a self) -> SocketRef<'a> {
         SocketRef(NonNull::new(*self).unwrap(), PhantomData)
+    }
+}
+
+#[doc(hidden)]
+pub mod wrappers {
+    use super::*;
+
+    pub fn SOCK_LOCK<'a>(so: &'a SocketRef<'_>) -> SockLockGuard<'a> {
+        so.sock_lock()
     }
 }
 
